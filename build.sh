9dd0f25c8f17d95ccc4367451155399a40594ada
Dirk Woellhaf <dirk.woellhaf@gmail.com>
2025-08-01 18:40:31 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ echo "GIT Commit: $(git describe --always --long)"
 echo "Building for Linux/AMD64..."
 podman run --rm \
 -v $PWD:/app docker.io/dirkw85/dev-golang:latest \
-env GOOS=linux GOARCH=amd64  go build -o teVis -ldflags "-X main.curVersion=$(git describe --always --long) -X 'main.curBuild=$(date)'" cmd/tevis/main.go 
-
+#env GOOS=linux GOARCH=amd64 go build -o teVis -ldflags "-X main.curVersion=$(git describe --always --long) -X 'main.curBuild=$(date)'" cmd/tevis/main.go 
+env GOOS=linux GOARCH=amd64 go build -o teVis -ldflags "-X main.curVersion=$(git rev-list --count HEAD)-$(git describe --always --long) -X 'main.curBuild=$(date)'" cmd/tevis/main.go 
 echo "Code Build completed!"
 
 podman build -f Dockerfile -t docker.io/dirkw85/tevis:$(git describe --always --long)
